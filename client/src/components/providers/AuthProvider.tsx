d796ffa28fa0067eadb459be4d0d34a5fccd2eb1
Arun Kumar Bind <arunkumarbind150720@gmail.com>
2026-04-19 18:41:01 +0530

    chore: upate AuthProvider.tsx

--- client/src/components/providers/AuthProvider.tsx
+++ client/src/components/providers/AuthProvider.tsx
@@ -4,12 +4,11 @@ import { useEffect } from "react";
 import { useAuthStore } from "@/lib/store/authStore";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const fetchMe = useAuthStore((state) => state.fetchMe);
-  const isLoading = useAuthStore((state) => state.isLoading);
+  const restoreSession = useAuthStore((state) => state.restoreSession);
 
   useEffect(() => {
-    fetchMe();
-  }, [fetchMe]);
+    restoreSession();
+  }, [restoreSession]);
 
   return <>{children}</>;
 }
